32db6d5ac82e410377f8f104d0828a6a743c4202
alivos7 <115407727+alivos7@users.noreply.github.com>
2022-10-11 12:50:04 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,171 @@
+<--085764229926--!>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hacked By Demon Alivos</title>
+<meta charset="UTF-8"/>
+<link rel="icon" type="https://i.ibb.co/L5YKckS/1631537858343.png">
+<meta property="og:image" content="https://i.ibb.co/L5YKckS/1631537858343.png">
+<meta name="description" content="Keep Silent"/>
+<meta name="description" content="Keep Silent">
+<meta name="author" content="Demon Alivos">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Iceberg" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+var snowmax=100var snowcolor=new Array("orange","red","darkred")var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")var snowletter="."var 
+sinkspeed=0.6var snowmaxsize=22var snowminsize=3var snowingzone=1
+var snow=new Array()var marginbottomvar marginrightvar timervar i_snow=0var x_mv=new Array();var crds=new Array();var lftrght=new Array();var browserinfos=navigator.userAgent 
+var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)var 
+ns6=document.getElementById&&!document.allvar opera=browserinfos.match(/Opera/) var browserok=ie5||ns6||opera
+function randommaker(range) {			
+rand=Math.floor(range*Math.random()) return rand}
+function initsnow() {	if (ie5 || opera) {		
+marginbottom = document.body.clientHeight		
+marginright = document.body.clientWidth	}	
+else if (ns6) {		
+marginbottom = window.innerHeight		
+marginright = window.innerWidth	}	
+var snowsizerange=snowmaxsize-snowminsize	
+for (i=0;i<=snowmax;i++) {		
+crds[i] = 0; 	
+lftrght[i] = Math.random()*15; 	
+x_mv[i] = 0.03 + Math.random()/10;		
+snow[i]=document.getElementById("s"+i)		
+snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]		
+snow[i].size=randommaker(snowsizerange)+snowminsize	
+snow[i].style.fontSize=snow[i].size		
+snow[i].style.color=snowcolor[randommaker(snowcolor.length)]		
+snow[i].sink=sinkspeed*snow[i].size/5		
+if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}		
+if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}		
+if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}		if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}		snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)		
+snow[i].style.left=snow[i].posx		
+snow[i].style.top=snow[i].posy	}movesnow()}
+function movesnow() {	
+for (i=0;i<=snowmax;i++) {		
+crds[i] += x_mv[i];		
+snow[i].posy+=snow[i].sink		
+snow[i].style.left=snow[i].posx+lftrght[i]*Math.sin(crds[i]);		
+snow[i].style.top=snow[i].posy			
+if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i])){			
+if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}			
+if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}			
+if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}			
+if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}			
+snow[i].posy=0		}	}	var timer=setTimeout("movesnow()",5)}
+for (i=0;i<=snowmax;i++) {	document.write("<span id='s"+i+"' style='position:absolute;top:-"+snowmaxsize+"'>"+snowletter+"</span>")}if (browserok) {	window.onload=initsnow}
+</script>
+<style type="text/css">		
+body{
+   background-color: black;   
+   font-family: 'Iceberg', sans-serif;
+ 	  }		
+
+.blink_me {
+animation: blinker 0.1s linear infinite;}		
+@keyframes blinker {50% {opacity: 0; }}		
+
+	.typewriter h1 {		 
+	color: #fff;		 
+	font-family: 'Iceberg', sans-serif;		 
+	overflow: hidden; 
+	/* Ensures the content is not revealed until the animation */		 
+	border-right: .25em solid orange; 
+	/* The typwriter cursor */		 
+	white-space: nowrap; 
+	/* Keeps the content on a single line */		 
+	margin: 0 auto; 
+	/* Gives that scrolling effect as the typing happens */		 
+	animation:typing 3.5s steps(30, end),blink-caret.5s step-end infinite;		
+	}				/* The typing effect */		
+	
+	@keyframes typing {		 
+	from { width: 0 }		
+	to { width: 100% }		
+	}				
+	/* The typewriter cursor effect */		
+	
+	@keyframes blink-caret {		 
+	from, to { border-color: transparent }		 
+	50% { border-color: red }		
+	}		
+	
+	hr {		 
+	border: 0;		 
+	border-top: 4px double #8c8c8c;		 
+	text-align: center;		
+	   }		
+	   
+	#q {font: 40px impact;
+	color:#ffffff;}	
+		
+		table, th, td {
+		border: 1px solid red;
+		}
+		</style>
+<script>
+		function checkTime(i) {
+
+if (i < 10) {
+
+i = "0" + i;
+
+}
+
+return i;
+
+}
+
+setInterval(showTime, 500);	
+function Mulailagucok(){ var audio = document.getElementById("lagu"); audio.play();
+}
+		</script>
+</head>
+<body>
+<br><center><img src="https://i.ibb.co/L5YKckS/1631537858343.png" style="width:50%;"/></center>
+<br>
+<center><font size="3" color="red">
+<b class="blink_me">I</b>
+<font size="2" color="red">
+<b class="blink_me">L</b>
+<font size="2" color="red">
+<b class="blink_me">O</b>
+<font size="2" color="red">
+<b class="blink_me">V</b>
+<font size="2" color="grey">
+<b class="blink_me">E</b> <font size="2" color="pink">
+<b class="blink_me">MyMood</b>
+<br>
+<font size="1" color="silver">
+<b class="blink_me"> Demon Alivos was here</b>
+</br>
+</center>
+</font>
+<br>
+<center><font size="1" color="silver">
+<b class="blink_me"></b> </center>
+</font>
+<center><font size="2" color="red">Nothing Stay's Hidden Forever </center>
+</font>
+<center>
+<font size="1" color="white">
+<b style=" color: white; text-shadow: 1px 1px 2px black, 0 0 25px red, 0 0 5px darkred;">Loyalty & Totally</b>
+<br>
+<font size="1" color="white">
+<b style=" color: blue; text-shadow: 1px 1px 2px red, 0 0 25px red, 0 0 5px darkred;">$Money~Searcher$</b>
+<font size="1" color="white">
+<b style=" color: orange; text-shadow: 1px 1px 2px red, 0 0 25px red, 0 0 5px darkred;">&</b>
+<font size="1" color="white">
+<b style=" color: blue; text-shadow: 1px 1px 2px red, 0 0 25px red, 0 0 5px darkred;">Ganest Seven</b>
+<br>
+<font size="1" color="white">
+<b style=" color: red; text-shadow: 1px 1px 2px black, 0 0 25px red, 0 0 5px darkred;">ᎠᎬᎷON ALIVOS || Demon No Kyo || Demon Emperor || Demon Amon || Demon King || Demon Gremory || || Demon Laxus || And All Family of Ganest Seven </b>
+<br>
+<audio id="lagu" src="https://nathanprinsley-files.prinsh.com/data-1/mp3/price-tag_putih-abu-abu.mp3"></audio>
+<button id="lagu" onclick="Mulailagucok();alert('Pake Headset Biar Enak')">Music </button>
+<br>
+</center>
+</body>
+</html>
